1aca1e6210743439360b545182162d0cbc49f3c3
willidv <willidv@gmail.com>
2017-09-06 15:38:09 -0500

    Update identical.py

--- identical.py
+++ identical.py
@@ -1,8 +1,8 @@
 def identical_strings(list_one, list_two):
-    for i in list_two:  
-        if i not in list_one:
-            print "the lists are different"
-            break   
+    for i in range(0,len(list_two)):  
+        if list_one[i] == list_two[i] :
+            print "the lists are same" 
         else:
-            print "the lists are the same"
-identical_strings(['celery','carrots','bread','milk'], ['celery','carrots','bread','cream'])
+            print "the lists are the different"
+            break
+identical_strings(['celery','carrots','bread','cream'], ['celery','carrots','bread','cream'])
